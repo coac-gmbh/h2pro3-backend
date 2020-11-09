b5cc5ffb0b541159b18015d83eae3a4850222250
kirilzilla <kirilm@me.com>
2020-11-09 14:03:02 +0100

    nexttravisfix

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,10 +2,6 @@
 eval "$(ssh-agent -s)";
 chmod 600 .travis/dokku-deploy.key;
 ssh-add .travis/dokku-deploy.key;
-
-DOKKU_HOST=52.59.241.198
-DOKKU_APP="okuna-test"
-
 ssh-keyscan "$DOKKU_HOST" >> ~/.ssh/known_hosts;
 git remote add deploy dokku@"$DOKKU_HOST":"$DOKKU_APP";
 git config --global push.default simple;
