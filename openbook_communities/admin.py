fa5d052173141659f64e1cc3bd2a356a6fcf545e
Edison Arango <edisonarab@gmail.com>
2021-03-02 02:23:05 -0500

    Feature/allow to assign categories to community from the admin

--- openbook_communities/admin.py
+++ openbook_communities/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
 from openbook_communities.models import Community
+from openbook_categories.models import Category
 
+class CategoryInline(admin.TabularInline):
+    model = Category.communities.through
 
 class CommunityAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        CategoryInline,
+    ]
 
 
 admin.site.register(Community, CommunityAdmin)
