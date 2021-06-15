15c0320d7915ee40cfac5588eaf792c3579e1bb4
Edwin Moreno <edwinmoreno328@gmail.com>
2021-06-15 18:42:07 -0500

    remove unnecessary code

--- openbook_auth/checkers.py
+++ openbook_auth/checkers.py
@@ -45,10 +45,6 @@ def check_can_post_to_community_with_name(user, community_name=None):
         raise ValidationError(
             _('You cannot post to a closed group where you\'re not an administrator'),
         )
-    if not user.can_create_post_to_community_with_name(community_name=community_name):
-        raise ValidationError(
-            _('You cannot post to a closed community where you\'re not an administrator'),
-        )
 
 
 def check_can_enable_disable_comments_for_post_in_community_with_name(user, community_name):
