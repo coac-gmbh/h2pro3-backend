5ef3c810e4b913a0ef6138ec066171fb4b07d7b5
Edison Arango <edisonarab@gmail.com>
2021-12-16 04:51:22 -0500

    Release/heroku redis SSL/TLS and Self-Signed certificates

--- openbook/settings.py
+++ openbook/settings.py
@@ -187,7 +187,8 @@ CACHES = {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": REDIS_DEFAULT_CACHE_LOCATION,
         "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "CONNECTION_POOL_KWARGS": {"ssl_cert_reqs": None}
         },
         "KEY_PREFIX": "ob-api-"
     },
@@ -195,7 +196,8 @@ CACHES = {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": REDIS_RQ_DEFAULT_JOBS_CACHE_LOCATION,
         "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "CONNECTION_POOL_KWARGS": {"ssl_cert_reqs": None}
         },
         "KEY_PREFIX": "ob-api-rq-default-job-"
     },
@@ -203,7 +205,8 @@ CACHES = {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": REDIS_RQ_HIGH_JOBS_CACHE_LOCATION,
         "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "CONNECTION_POOL_KWARGS": {"ssl_cert_reqs": None}
         },
         "KEY_PREFIX": "ob-api-rq-high-job-"
     },
@@ -211,7 +214,8 @@ CACHES = {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": REDIS_RQ_LOW_JOBS_CACHE_LOCATION,
         "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "CONNECTION_POOL_KWARGS": {"ssl_cert_reqs": None}
         },
         "KEY_PREFIX": "ob-api-rq-low-job-"
     },
